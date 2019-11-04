c492dddf7822659914c58c853831213410bc1055
Jorge Barcos <jorgebarcos88@gmail.com>
2019-11-04 09:29:04 -0300

    Instalando Mongoose y conectando a la base de datos

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose');
+require('./config/db');
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const router = require('./routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+require('dotenv').config({ path: 'variables.env' });
 
 const app = express();
 
@@ -17,6 +25,18 @@ app.set('view engine', 'handlebars');
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser);
+
+app.use(
+	session({
+		secret: process.env.SECRETO,
+		key: process.env.KEY,
+		resave: false,
+		saveUninitialized: false,
+		store: new MongoStore({ mongooseConnection: mongoose.connection })
+	})
+);
+
 app.use('/', router());
 
-app.listen(5000);
+app.listen(process.env.PUERTO);
