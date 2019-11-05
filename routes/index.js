9cf621e83129bf11b56e03eb03bc320c10bc35eb
Jorge Barcos <jorgebarcos88@gmail.com>
2019-11-05 18:07:44 -0300

    Almacenando la vacante en la base de datos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ module.exports = () => {
 
 	// crear vacantes
 	router.get('/vacantes/nueva', vacantesController.formularioNuevaVacante);
+	router.post('/vacantes/nueva', vacantesController.agregarVacante);
 
 	return router;
 };
