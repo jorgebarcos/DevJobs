920c469374dddd135735b40bb9fcd05dda8b7754
Jorge Barcos <jorgebarcos88@gmail.com>
2019-12-16 09:30:08 -0300

    Mostrando los Errores en Alertas

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -41,5 +41,19 @@ module.exports = {
 	},
 	tipoContrato: (seleccionado, opciones) => {
 		return opciones.fn(this).replace(new RegExp(`value="${seleccionado}"`), '$& selected="selected"');
+	},
+	mostrarAlertas: (errores = {}, alertas) => {
+		const categoria = Object.keys(errores);
+
+		let html = '';
+		if (categoria.length) {
+			errores[categoria].forEach((error) => {
+				html += `<div class="${categoria} alerta">
+					${error}
+					</div>
+				`;
+			});
+		}
+		return (alertas.fn().html = html);
 	}
 };
