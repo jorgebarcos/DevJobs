ab90bf5807c0d8d094dc3bf87d1862742294c23c
Jorge Barcos <jorgebarcos88@gmail.com>
2019-12-16 11:44:36 -0300

    Mostrando las vacantes en el Panel de Administración

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
+const Vacante = mongoose.model('Vacante');
 
 exports.autenticarUsuario = passport.authenticate('local', {
     successRedirect: '/administracion',
@@ -18,10 +20,14 @@ exports.verificarUsuario = (req, res, next) => {
     res.redirect('/iniciar-sesion')
 }
 
-exports.mostrarPanel = (req, res) => {
+exports.mostrarPanel = async (req, res) => {
+
+    // consultar el usuario autenticado
+    const vacantes = await Vacante.find({ autor: req.user._id })
+
     res.render('administracion', {
         nombrePagina: 'Panel de Administracion',
-        tagline: 'Crea y Administra tus vacantes desde aquí'
+        tagline: 'Crea y Administra tus vacantes desde aquí', vacantes
     }
     )
 }
