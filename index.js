f6ea666b3c1d3a16c4981cead8986528bdf518a1
Jorge Barcos <jorgebarcos88@gmail.com>
2019-12-24 18:21:37 -0300

    Manejo de Errores

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const MongoStore = require('connect-mongo')(session);
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator')
 const flash = require('connect-flash')
+const createError = require('http-errors')
 const passport = require('./config/passport')
 
 
@@ -65,5 +66,21 @@ app.use((req, res, next) => {
 })
 
 app.use('/', router());
+
+// 404 pagina no existente
+app.use((req, res, next) =>  {
+	next(createError(404, 'No Encontrado'))
+})
+
+// Administración de los errores
+app.use((error, req, res) => {
+	res.locals.mensaje = error.message;
+	const status = error.status || 500;
+	res.locals.status = status;
+	res.status(status);
+	res.render('error')
+})
+
+
 
 app.listen(process.env.PUERTO);
